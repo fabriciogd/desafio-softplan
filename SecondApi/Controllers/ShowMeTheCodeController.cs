2509e80109885439f11bacaee6d3fea884159e9a
Fabricio Gabriel Dupont <fdupont@grupoa.com.br>
2021-01-08 01:44:12 -0300

    Controller de showmethecode

---
+++ SecondApi/Controllers/ShowMeTheCodeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SecondApi.Controllers
+{
+    [ApiController]
+    [Route("showmethecode")]
+    public class ShowMeTheCodeController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            string url = "https://github.com/fabriciogd/desafio-softplan";
+
+            return url;
+        }
+    }
+}
